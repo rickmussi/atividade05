ce163f3fd5f3c5d97aa5b9d68fbdb2f391e23b80
rickmussi <rickmussi@hotmail.com>
2018-05-15 20:01:46 -0300

    Primeiro envio

--- at6Exercicio5/src/test/java/com/utfpr/cp/at6exe5/at6exercicio5/SomatoriaTest.java
+++ at6Exercicio5/src/test/java/com/utfpr/cp/at6exe5/at6exercicio5/SomatoriaTest.java
@@ -7,19 +7,41 @@ package com.utfpr.cp.at6exe5.at6exercicio5;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
  * @author Mussi
  */
 public class SomatoriaTest {
-    
+
     public SomatoriaTest() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void somaDeFatoriais2() {
+        MathOps mathOpsMock = mock(MathOps.class);
+        Primo primoMock = mock(Primo.class);
+
+        int n = 0, retorno = 3628920;
+        int[] numeros = {5, 10};
+
+        when(primoMock.ehPrimo(5))
+                .thenReturn(true);
+
+        when(primoMock.ehPrimo(10))
+                .thenReturn(false);
+
+        when(mathOpsMock.fatorial(5))
+                .thenReturn(120);
+
+        when(mathOpsMock.fatorial(10))
+                .thenReturn(3628800);
+
+        Somatoria s = new Somatoria(mathOpsMock);
+        assertEquals(3628800, s.somaDeFatoriais(numeros, primoMock));
+    }
+
+
 }
